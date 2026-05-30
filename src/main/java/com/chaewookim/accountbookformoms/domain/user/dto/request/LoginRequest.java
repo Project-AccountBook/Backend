fc3b7f2d69610e7b83c81bc82421d84aec5dc78e
rlatjddms <kseo_o@naver.com>
2026-05-30 18:45:30 +0900

    [feat] 로그인 dto 구현

--- src/main/java/com/chaewookim/accountbookformoms/domain/user/dto/request/LoginRequest.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/user/dto/request/LoginRequest.java
@@ -5,14 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest (
 
-    @NotBlank
-    String username,
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        String email,
 
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
-    String password
+        @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+        String password
 ) {
-    public LoginRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 }
--- src/main/java/com/chaewookim/accountbookformoms/domain/user/dto/response/LoginResponse.java
+++
@@ -1,9 +0,0 @@
-package com.chaewookim.accountbookformoms.domain.user.dto.response;
-
-public record LoginResponse (
-
-    String accessToken,
-    String refreshToken,
-    String username
-) {
-}
