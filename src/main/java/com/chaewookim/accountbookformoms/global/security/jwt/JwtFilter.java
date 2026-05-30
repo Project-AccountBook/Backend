0729d4e73d582b87aa6966becad5f192cca91def
rlatjddms <kseo_o@naver.com>
2026-05-30 19:14:57 +0900

    [refactor] JwtFilter 파라미터에 @NonNull 어노테이션 추가

--- src/main/java/com/chaewookim/accountbookformoms/global/security/jwt/JwtFilter.java
+++ src/main/java/com/chaewookim/accountbookformoms/global/security/jwt/JwtFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -20,7 +21,11 @@ public class JwtFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
 
         // 1. Request Header에서 토큰 추출
         String token = resolveToken(request);
