234870f48c90e6d09ebaab1ad2710acaae4b0861
rlatjddms <kseo_o@naver.com>
2026-05-30 18:42:14 +0900

    [feat] 회원가입 api 구현

--- src/main/java/com/chaewookim/accountbookformoms/global/event/UserSignedUpEvent.java
+++ src/main/java/com/chaewookim/accountbookformoms/global/event/UserSignedUpEvent.java
@@ -3,6 +3,7 @@ package com.chaewookim.accountbookformoms.global.event;
 import jakarta.validation.constraints.NotNull;
 
 public record UserSignedUpEvent(
+
         @NotNull
         Long userId
 ) {}
