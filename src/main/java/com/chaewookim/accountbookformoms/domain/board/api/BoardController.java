81987fd246eec6630780648946b51ddde8b1efa0
chaewookim <chaewookim2877@gmail.com>
2026-06-01 21:12:05 +0900

    [feat] #21 Board entity 구현

---
+++ src/main/java/com/chaewookim/accountbookformoms/domain/board/api/BoardController.java
@@ -0,0 +1,4 @@
+package com.chaewookim.accountbookformoms.domain.board.api;
+
+public class BoardController {
+}
---
+++ src/main/java/com/chaewookim/accountbookformoms/domain/board/enums/BOARD_TYPE.java
@@ -0,0 +1,4 @@
+package com.chaewookim.accountbookformoms.domain.board.enums;
+
+public enum BOARD_TYPE {
+}
