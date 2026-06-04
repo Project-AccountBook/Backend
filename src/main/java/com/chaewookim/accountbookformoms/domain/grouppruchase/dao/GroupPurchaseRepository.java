8fc7327698ebb6e9ee1071a15b31161bfb800a6f
Ongsaem0 <yeahrim97@naver.com>
2026-06-04 18:56:41 +0900

    [feat] #6 공동구매 전체 조회 기능 구현 (#54)
    공동구매 전체 조회 기능 구현

--- src/main/java/com/chaewookim/accountbookformoms/domain/grouppruchase/dao/GroupPurchaseRepository.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/grouppruchase/dao/GroupPurchaseRepository.java
@@ -1,9 +1,24 @@
 package com.chaewookim.accountbookformoms.domain.grouppruchase.dao;
 
 import com.chaewookim.accountbookformoms.domain.grouppruchase.domain.GroupPurchase;
+import com.chaewookim.accountbookformoms.domain.grouppruchase.domain.enums.PurchaseStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GroupPurchaseRepository extends JpaRepository<GroupPurchase, Long> {
+
+    @Query("SELECT gp FROM GroupPurchase gp " +
+           "WHERE gp.status = :status " +
+           "AND (:region IS NULL OR gp.pickupLocation LIKE %:region%)")
+    List<GroupPurchase> findActiveGroupPurchases(
+            @Param("status") PurchaseStatus status,
+            @Param("region") String region,
+            Sort sort
+    );
 }
