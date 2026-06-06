77ec45afa38f950dcb8c600a5019e507b1097f15
rlatjddms <kseo_o@naver.com>
2026-06-06 14:39:39 +0900

    [feat] #60 Category 엔티티 구현

---
+++ src/main/java/com/chaewookim/accountbookformoms/domain/asset/entity/Category.java
@@ -0,0 +1,51 @@
+package com.chaewookim.accountbookformoms.domain.asset.entity;
+
+import com.chaewookim.accountbookformoms.domain.asset.enums.TransactionType;
+import com.chaewookim.accountbookformoms.domain.user.entity.User;
+import com.chaewookim.accountbookformoms.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE category SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Builder
+    public Category(User user, TransactionType type, String name) {
+        this.user = user;
+        this.type = type;
+        this.name = name;
+    }
+}
