ead6420c64cea19c3d0c7e6ea3a736af1fe5bf12
rlatjddms <kseo_o@naver.com>
2026-06-06 16:56:48 +0900

    [feat] #36 알림 생성 및 관리 서비스 로직 구현

---
+++ src/main/java/com/chaewookim/accountbookformoms/domain/notification/dao/NotificationRepository.java
@@ -0,0 +1,23 @@
+package com.chaewookim.accountbookformoms.domain.notification.dao;
+
+import com.chaewookim.accountbookformoms.domain.notification.entity.Notification;
+import com.chaewookim.accountbookformoms.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findByUser(User user, Pageable pageable);
+
+    Long countByUserAndIsReadFalse(User user);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isRead = true WHERE n.user.id = :userId AND n.isRead = false")
+    void markAllAsReadByUserId(@Param("userId") Long userId);
+}
