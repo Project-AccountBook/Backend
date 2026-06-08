7224a85ba99d39293abd79eaa277bceb6819e528
rlatjddms <kseo_o@naver.com>
2026-06-08 13:19:56 +0900

    [feat] #36 fcm json 파일 추가 및 FirebaseConfig 생성

---
+++ src/main/java/com/chaewookim/accountbookformoms/global/config/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package com.chaewookim.accountbookformoms.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("classpath:jointliving-notification.json")
+    private Resource serviceAccount;
+
+    @PostConstruct
+    public void init() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+    }
+}
