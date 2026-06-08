524fe6a8311233932e77e4940f704b84d4b39bf0
rlatjddms <kseo_o@naver.com>
2026-06-08 12:21:55 +0900

    [chore] #60 Asset 도메인 커스텀 에러코드 통합

--- src/main/java/com/chaewookim/accountbookformoms/domain/asset/error/AssetErrorCode.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/asset/error/AssetErrorCode.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AssetErrorCode implements BaseErrorCode {
 
+    // Account
+    ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "계좌를 찾을 수 없습니다."),
+
     // FixedTransaction
     INVALID_REPEAT_DAY(HttpStatus.BAD_REQUEST, "반복 일자는 1~31일 사이여야 합니다.");
 
