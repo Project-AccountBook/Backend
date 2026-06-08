a4b55fe5ac8a9c63172285162044d2b8593c5767
rlatjddms <kseo_o@naver.com>
2026-06-08 13:22:28 +0900

    [feat] #36 메인 어플리케이션에 @EnableAsync 추가

--- src/main/java/com/chaewookim/accountbookformoms/AccountBookForMomsApplication.java
+++ src/main/java/com/chaewookim/accountbookformoms/AccountBookForMomsApplication.java
@@ -3,9 +3,11 @@ package com.chaewookim.accountbookformoms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class AccountBookForMomsApplication {
 
     public static void main(String[] args) {
