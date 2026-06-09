7e19e878c3e297c7ab531811c800aa1e6931cc36
rlatjddms <kseo_o@naver.com>
2026-06-09 10:25:56 +0900

    [feat] #40 카테고리 관리 서비스 코드 구현

---
+++ src/main/java/com/chaewookim/accountbookformoms/domain/asset/application/CategoryService.java
@@ -0,0 +1,63 @@
+package com.chaewookim.accountbookformoms.domain.asset.application;
+
+import com.chaewookim.accountbookformoms.domain.asset.dao.TransactionCategoryRepository;
+import com.chaewookim.accountbookformoms.domain.asset.dto.request.CategoryRequest;
+import com.chaewookim.accountbookformoms.domain.asset.dto.response.CategoryResponse;
+import com.chaewookim.accountbookformoms.domain.asset.entity.TransactionCategory;
+import com.chaewookim.accountbookformoms.domain.asset.error.AssetErrorCode;
+import com.chaewookim.accountbookformoms.domain.user.entity.User;
+import com.chaewookim.accountbookformoms.global.error.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CategoryService {
+
+    private final TransactionCategoryRepository categoryRepository;
+
+    public List<CategoryResponse> getCategories(Long userId) {
+        return categoryRepository.findAllByUserOrSystem(userId).stream()
+                .map(CategoryResponse::from)
+                .toList();
+    }
+
+    @Transactional
+    public CategoryResponse createCustomCategory(User user, CategoryRequest request) {
+        TransactionCategory savedCategory = categoryRepository.save(request.toEntity(user));
+        return CategoryResponse.from(savedCategory);
+    }
+
+    @Transactional
+    public void updateCategory(Long categoryId, Long userId, CategoryRequest request) {
+        TransactionCategory category = validateAndGetCategory(categoryId, userId);
+        category.update(request.name(), request.type());
+    }
+
+    @Transactional
+    public void deleteCategory(Long categoryId, Long userId) {
+        TransactionCategory category = validateAndGetCategory(categoryId, userId);
+        categoryRepository.delete(category);
+    }
+
+    // 공통 검증 로직
+    private TransactionCategory validateAndGetCategory(Long categoryId, Long userId) {
+
+        TransactionCategory category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new CustomException(AssetErrorCode.CATEGORY_NOT_FOUND));
+
+        if (category.getUser() == null) {
+            throw new CustomException(AssetErrorCode.CATEGORY_IMMUTABLE);
+        }
+
+        if (!category.getUser().getId().equals(userId)) {
+            throw new CustomException(AssetErrorCode.CATEGORY_FORBIDDEN);
+        }
+
+        return category;
+    }
+}
