6f7b8ffcea506c5a771969153beb94a1e6638fd6
rlatjddms <kseo_o@naver.com>
2026-06-09 10:26:08 +0900

    [feat] #40 카테고리 커스텀 에러 코드 추가

--- src/main/java/com/chaewookim/accountbookformoms/domain/asset/error/AssetErrorCode.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/asset/error/AssetErrorCode.java
@@ -13,7 +13,12 @@ public enum AssetErrorCode implements BaseErrorCode {
     ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "계좌를 찾을 수 없습니다."),
 
     // FixedTransaction
-    INVALID_REPEAT_DAY(HttpStatus.BAD_REQUEST, "반복 일자는 1~31일 사이여야 합니다.");
+    INVALID_REPEAT_DAY(HttpStatus.BAD_REQUEST, "반복 일자는 1~31일 사이여야 합니다."),
+
+    // Category
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다."),
+    CATEGORY_FORBIDDEN(HttpStatus.FORBIDDEN, "본인의 카테고리만 수정/삭제 가능합니다."),
+    CATEGORY_IMMUTABLE(HttpStatus.BAD_REQUEST, "기본 카테고리는 수정하거나 삭제할 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
