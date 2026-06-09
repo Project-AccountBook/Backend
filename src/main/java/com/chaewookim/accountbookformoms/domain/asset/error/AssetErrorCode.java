1b8dc0d9eb747a716c3e1f60e99bc50dc2e45347
rlatjddms <kseo_o@naver.com>
2026-06-09 17:48:37 +0900

    [feat] #42 거래 내역 커스텀 에러 코드 추가

--- src/main/java/com/chaewookim/accountbookformoms/domain/asset/error/AssetErrorCode.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/asset/error/AssetErrorCode.java
@@ -11,6 +11,11 @@ public enum AssetErrorCode implements BaseErrorCode {
 
     // Account
     ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "계좌를 찾을 수 없습니다."),
+    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
+
+    // Transaction
+    TRANSACTION_NOT_FOUND(HttpStatus.NOT_FOUND, "거래 내역을 찾을 수 없습니다."),
+    TRANSACTION_FORBIDDEN(HttpStatus.FORBIDDEN, "본인의 거래 내역만 접근할 수 있습니다."),
 
     // FixedTransaction
     INVALID_REPEAT_DAY(HttpStatus.BAD_REQUEST, "반복일은 1일부터 31일 사이여야 합니다."),
