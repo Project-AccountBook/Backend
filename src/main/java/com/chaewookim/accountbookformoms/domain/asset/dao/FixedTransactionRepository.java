3f69d0cbe72c1598167ea42f4eaf9e696bd8ef64
rlatjddms <kseo_o@naver.com>
2026-06-11 15:36:24 +0900

    [feat] #69 고정 거래 스케줄러 구현

--- src/main/java/com/chaewookim/accountbookformoms/domain/asset/dao/FixedTransactionRepository.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/asset/dao/FixedTransactionRepository.java
@@ -3,9 +3,11 @@ package com.chaewookim.accountbookformoms.domain.asset.dao;
 import com.chaewookim.accountbookformoms.domain.asset.entity.FixedTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface FixedTransactionRepository extends JpaRepository<FixedTransaction, Long> {
 
     List<FixedTransaction> findAllByUserId(Long userId);
+    List<FixedTransaction> findAllByIsActiveTrueAndNextExecutionDateLessThanEqual(LocalDate today);
 }
