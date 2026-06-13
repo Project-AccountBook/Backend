fa9c88a0c1af43f5ecfb079cdeb16ea0584a1ffb
rlatjddms <kseo_o@naver.com>
2026-06-13 14:18:35 +0900

    [feat] #44 예산 관리 dto 구현

---
+++ src/main/java/com/chaewookim/accountbookformoms/domain/budget/dto/response/BudgetResponse.java
@@ -0,0 +1,17 @@
+package com.chaewookim.accountbookformoms.domain.budget.dto.response;
+
+import java.math.BigDecimal;
+
+public record BudgetResponse(
+
+        Long id,
+        Long categoryId,
+        String categoryName,
+        BigDecimal totalBudget,
+        BigDecimal expectedExpense,
+        BigDecimal totalPlannedBudget,
+        BigDecimal actualExpense,
+        BigDecimal remainingBudget,
+        BigDecimal progress
+) {
+}
