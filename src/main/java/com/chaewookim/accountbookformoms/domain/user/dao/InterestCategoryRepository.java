b27c8d1c7700f5134cba6fba1c6bf5f483fe58e7
rlatjddms <kseo_o@naver.com>
2026-06-14 15:11:25 +0900

    [feat] #94 관심 카테고리 관리 서비스 코드 구현

--- src/main/java/com/chaewookim/accountbookformoms/domain/user/dao/InterestCategoryRepository.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/user/dao/InterestCategoryRepository.java
@@ -2,11 +2,16 @@ package com.chaewookim.accountbookformoms.domain.user.dao;
 
 import com.chaewookim.accountbookformoms.domain.grouppruchase.domain.Category;
 import com.chaewookim.accountbookformoms.domain.user.entity.InterestCategory;
+import com.chaewookim.accountbookformoms.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InterestCategoryRepository extends JpaRepository<InterestCategory, Long> {
 
     List<InterestCategory> findByCategoryAndIsAlarmEnabledTrue(Category category);
+    List<InterestCategory> findByUserId(Long userId);
+    boolean existsByUserAndCategory(User user, Category category);
+    Optional<InterestCategory> findByIdAndUserId(Long id, Long userId);
 }
