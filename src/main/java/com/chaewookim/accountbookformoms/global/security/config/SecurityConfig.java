4a521458e90ecc4f97c0ffb4911c21f55f944dff
rlatjddms <kseo_o@naver.com>
2026-07-04 22:35:45 +0900

    [fix] #107 대시보드 캐시 문제 해결

--- src/main/java/com/chaewookim/accountbookformoms/global/security/config/SecurityConfig.java
+++ src/main/java/com/chaewookim/accountbookformoms/global/security/config/SecurityConfig.java
@@ -84,7 +84,10 @@ public class SecurityConfig {
                                 "/api/v1/transactions/**",
 
                                 // fixedTransaction 부분
-                                "/api/v1/fixed-transactions/**"
+                                "/api/v1/fixed-transactions/**",
+
+                                // category 부분
+                                "/api/v1/categories/**"
                         ).authenticated()
 
                         // 그 외 모든 요청은 인증 필요
