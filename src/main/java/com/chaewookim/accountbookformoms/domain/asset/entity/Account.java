afb18ccd0f4c591357d4c75c1e6c143ca67e3f55
rlatjddms <kseo_o@naver.com>
2026-07-11 13:57:16 +0900

    [feat] #117 계좌 역할 및 목표 컬럼과 enum 추가

--- src/main/java/com/chaewookim/accountbookformoms/domain/asset/entity/Account.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/asset/entity/Account.java
@@ -1,11 +1,14 @@
 package com.chaewookim.accountbookformoms.domain.asset.entity;
 
+import com.chaewookim.accountbookformoms.domain.asset.enums.AccountRole;
 import com.chaewookim.accountbookformoms.domain.asset.error.AssetErrorCode;
 import com.chaewookim.accountbookformoms.domain.user.entity.User;
 import com.chaewookim.accountbookformoms.global.entity.BaseEntity;
 import com.chaewookim.accountbookformoms.global.error.CustomException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +23,7 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -45,12 +49,23 @@ public class Account extends BaseEntity {
     @Column(nullable = false)
     private BigDecimal currentBalance;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountRole role = AccountRole.CHECKING;
+
+    @Column
+    private BigDecimal goalAmount;
+
+    @Column
+    private LocalDate goalDate;
+
     @Builder
-    public Account(User user, String accountName, BigDecimal initialBalance) {
+    public Account(User user, String accountName, BigDecimal initialBalance, AccountRole role) {
         this.user = user;
         this.accountName = accountName;
         this.initialBalance = initialBalance;
         this.currentBalance = initialBalance;
+        this.role = role != null ? role : AccountRole.CHECKING;
     }
 
     public void updateAccountName(String accountName) {
@@ -73,5 +88,22 @@ public class Account extends BaseEntity {
             throw new CustomException(AssetErrorCode.INSUFFICIENT_BALANCE);
         }
         this.currentBalance = this.currentBalance.add(amount);
+    }
+
+    public void updateRole(AccountRole role) {
+        this.role = role != null ? role : AccountRole.CHECKING;
+    }
+
+    public void updateGoal(BigDecimal goalAmount, LocalDate goalDate) {
+        if (goalAmount != null && goalAmount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new CustomException(AssetErrorCode.INVALID_GOAL_AMOUNT);
+        }
+        this.goalAmount = goalAmount;
+        this.goalDate = goalDate;
+    }
+
+    public void clearGoal() {
+        this.goalAmount = null;
+        this.goalDate = null;
     }
 }
