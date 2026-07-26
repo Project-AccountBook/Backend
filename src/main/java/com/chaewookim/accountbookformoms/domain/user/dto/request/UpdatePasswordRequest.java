e950e923b49036420cf3ef82e31ac8e75f2f5631
rlatjddms <kseo_o@naver.com>
2026-07-26 15:07:53 +0900

    [fix] #130 비밀번호 설정 문구 수정

--- src/main/java/com/chaewookim/accountbookformoms/domain/user/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/chaewookim/accountbookformoms/domain/user/dto/request/UpdatePasswordRequest.java
@@ -9,7 +9,7 @@ public record UpdatePasswordRequest(
 
         @NotBlank(message = "새로운 비밀번호는 필수 입력값입니다.")
         @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
-                message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+                message = "비밀번호는 8~16자 영문, 숫자, 특수문자 조합이어야 합니다.")
         String newPassword
 ) {
 }
